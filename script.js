cf45077f3bc3903919168bd55faf088fca1cd171
Basil Skaria <basilskar1960@gmail.com>
2024-08-07 23:02:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+       const sr = ScrollReveal();
+    sr.reveal('#profile', {
+        duration: 3000, // Animation duration (in milliseconds)
+        origin: 'left', // Slide in from the left
+        distance: '100px', // Distance to slide
+    });
+    sr.reveal('#experience', {
+        duration: 3000, // Animation duration (in milliseconds)
+        origin: 'left', // Slide in from the left
+        distance: '100px', // Distance to slide
+    });
+    sr.reveal('#contact', {
+        duration: 3000, // Animation duration (in milliseconds)
+        origin: 'left', // Slide in from the left
+        distance: '100px', // Distance to slide
+    });
+    sr.reveal('#projects', {
+        duration: 3000, // Animation duration (in milliseconds)
+        origin: 'left', // Slide in from the left
+        distance: '100px', // Distance to slide
+    });
+    sr.reveal('#about', {
+        duration: 5000, // Animation duration (in milliseconds)
+        origin: 'left', // Slide in from the left
+        distance: '100px', // Distance to slide
+    });
+  
